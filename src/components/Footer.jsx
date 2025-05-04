668bc2b041fa69ed21a765281d0210029619b1b0
MOHAMED <ahmadiin612@gmail.com>
2025-05-04 22:00:36 +0300

    centerd box

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 export function Footer() {
   return(
     <footer className="bg-orange-300">
-       <h1>Admission process</h1>
+          <div class="flex items-center space-x-4">
+              <div class="flex-1 h-px bg-gray-400"></div>
+              <div class="px-4 py-2 bg-blue-500 text-white rounded">Centered Box</div>
+              <div class="flex-1 h-px bg-gray-400"></div>
+          </div>
     </footer>
   )
 }
