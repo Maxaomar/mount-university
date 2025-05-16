5cd527f9b56d0b9c51443ab957b1831f269d8f36
MOHAMED <ahmadiin612@gmail.com>
2025-05-16 22:05:23 +0300

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="bg-orange-300">
           <div class="flex items-center space-x-4">
               <div class="flex-1 h-px bg-lineColor"></div>
-              <div class="px-4 py-2 bg-blue-500 text-white rounded">Centered Box</div>
+              <div class="px-4 py-2 bg-blue-500 text-white rounded">Admission process</div>
               <div className="flex-1 h-1 bg-lineColor"></div>
           </div>
     </footer>
---
+++ src/footer/AdmProcess.jsx
@@ -0,0 +1,13 @@
+export function AdmissionProcess() {
+    return(
+        <article>
+            <div>
+                <i class="fa-brands fa-telegram"></i>
+                <h1>Application</h1>
+                <p>Submit your application form and supporting documents online or at our offices.</p>
+            </div>
+            <div></div>
+            <div></div>
+        </article>
+    )
+}
