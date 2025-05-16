0d0fc16987724c736f21e1ff30b9bdd5c4878d39
MOHAMED <ahmadiin612@gmail.com>
2025-05-16 22:16:39 +0300

    added new file

--- src/footer/AdmProcess.jsx
+++ src/footer/AdmProcess.jsx
@@ -6,8 +6,25 @@ export function AdmissionProcess() {
                 <h1>Application</h1>
                 <p>Submit your application form and supporting documents online or at our offices.</p>
             </div>
-            <div></div>
-            <div></div>
+
+            <div>
+                <i class="fa-regular fa-square-check"></i>
+                <h1>Verification</h1>
+                <p>Authentication of the submitted academic and identification documents.</p>
+            </div>
+
+
+            <div>
+                <i class="fa-solid fa-camera"></i>
+                <h1>Review</h1>
+                <p>The application is compared with the chosen course entry requirements.</p>
+            </div>
+
+            <div>
+                <i class="fa-regular fa-face-smile"></i>
+                <h1>Reporting</h1>
+                <p>communication to applicant on admission status .</p>
+            </div>
         </article>
     )
 }
