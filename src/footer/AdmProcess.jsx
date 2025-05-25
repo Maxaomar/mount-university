57c6645eebd980680bc1d00c73114029c1f9b0c9
MOHAMED <ahmadiin612@gmail.com>
2025-05-25 12:16:00 +0300

    styled admission process

--- src/footer/AdmProcess.jsx
+++ src/footer/AdmProcess.jsx
@@ -15,32 +15,39 @@ export function AdmissionProcess() {
     return(
         <article className="">
             <TitleBox />
-        <div className="flex">
-            <div className="flex flex-col">
-                <div className="flex items-center gap-2 text-3xl bg-white">
+        <div className="flex ml-20 mr-20">
+
+            <div className="flex flex-col text-3xl">
+                <div className="flex items-center gap-2">
                     <i className="fa-brands fa-telegram pt-2"></i>
-                    <h1 className="text-2xl">Application</h1>
+                    <h1 className="text-2xl font-bold">Application</h1>
                 </div>
                 <p className="ml-7 text-sm">Submit your application form and supporting documents online or at our offices.</p>
             </div>
 
-            <div>
-                <i class="fa-regular fa-square-check"></i>
-                <h1>Verification</h1>
-                <p>Authentication of the submitted academic and identification documents.</p>
+            <div className="flex flex-col text-3xl">
+                <div className="flex items-center gap-2">
+                    <i className="fa-regular fa-square-check pt-2"></i>
+                    <h1 className="font-bold text-2xl">Verification</h1>
+                </div>
+                <p className="text-sm ml-7">Authentication of the submitted academic and identification documents.</p>
             </div>
 
 
-            <div>
-                <i class="fa-solid fa-camera"></i>
-                <h1>Review</h1>
-                <p>The application is compared with the chosen course entry requirements.</p>
+            <div className="flex flex-col text-3xl">
+                <div className="flex items-center gap-2">
+                    <i className="fa-solid fa-camera pt-2"></i>
+                    <h1 className="font-bold text-2xl">Review</h1>
+                </div>
+                <p className="text-sm ml-7">The application is compared with the chosen course entry requirements.</p>
             </div>
 
-            <div>
-                <i class="fa-regular fa-face-smile"></i>
-                <h1>Reporting</h1>
-                <p>communication to applicant on admission status .</p>
+            <div className="flex flex-col text-3xl">
+                <div className="flex items-center gap-2">
+                    <i className="fa-regular fa-face-smile pt-2"></i>
+                    <h1 className="text-2xl font-bold">Reporting</h1>
+                </div>
+                <p className="ml-7 text-sm">communication to applicant on admission status .</p>
             </div>
          </div>
      </article>
