c372ba665e602ae8e2458ad1e63ac7fe8c134c7f
MOHAMED <ahmadiin612@gmail.com>
2025-05-28 21:41:01 +0300

    lines practice

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
-export function Home(params) {
+export function Home() {
   return(
     <>
-    <div className="divider">
-      <span>Unlocking infinite possibilities</span>
-    </div>
+      <div className="flex items-center space-x-4">
+            <div className="flex-1 bg-green-400 h-1"></div>
+            <div className="px-4 py-2 rounded">
+                     <i class="fa-solid fa-circle-half-stroke"></i>
+                     Unlocking infinite possibilities
+            </div>
+            <div className=" flex-1 bg-green-400 h-1"></div>
+      </div>
     </>
   )
 }
