8c4a8937eb784d0e42bbe25607a42d5e49bd9a7c
MOHAMED <ahmadiin612@gmail.com>
2025-05-28 21:48:40 +0300

    lines practice

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,12 @@ export function Home() {
   return(
     <>
       <div className="flex items-center space-x-4">
-            <div className="flex-1 bg-green-400 h-1"></div>
-            <div className="px-4 py-2 rounded">
-                     <i class="fa-solid fa-circle-half-stroke"></i>
+            <div className="flex-1  bg-orange-200 h-1"></div>
+            <div className="px-0 py-2 rounded">
+                     <i className="fa-solid fa-circle-half-stroke text-blue-700"></i>
                      Unlocking infinite possibilities
             </div>
-            <div className=" flex-1 bg-green-400 h-1"></div>
+            <div className=" flex-1 bg-orange-200 h-1"></div>
       </div>
     </>
   )
